cab50029bcaf38018d13c0d7c0c2564bd59d6367
AkashVarsani <akashvarsani15399@gmail.com>
2025-04-05 17:04:29 +0530

    Adding Meta Tags

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        property="og:title"
+        content="CareerTrack - Your Pathway to a Bright Future"
+      />
+      <meta
+        property="og:description"
+        content="Discover the best career options, resources, skill assessments, and community to guide you from 10th grade to graduation."
+      />
+      <meta
+        property="og:image"
+        content="https://career-track-psi.vercel.app/logo.ico"
+      />
+      <meta property="og:url" content="https://career-track-psi.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="CareerTrack" />
+      <meta property="og:locale" content="en_US" />
       <body
         className={`${poppins.variable} ${geistSans.variable} ${geistMono.variable} font-poppins antialiased`}
       >
